81bcb282786cc7933e8356f07cfbbe4e521e6bc9
Aljaz Ceru <aljaz@ceru.si>
2026-04-23 06:33:38 +0200

    chore: bump version to 0.2.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.disobey.mango"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.2"
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
         }
